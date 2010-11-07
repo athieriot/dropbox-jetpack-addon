49759dbf76eae070f63ea0fb11cba10a32b85a2d
Aurélien Thieriot <a.thieriot@gmail.com>
2010-11-08 00:58:17 +0100

    its hard to have a oauth signature

--- tests/test-dropbox-lib.js
+++ tests/test-dropbox-lib.js
@@ -1,5 +1,31 @@
 var dropbox = require("dropbox-lib");
 
+var clientData = {
+	"token": "jxjxmvxyaiwjvt9",
+        "secret": "34s78vlfshegojm"
+};
+
 exports.ensureWhoDropBoxLibIs = function(test) {
-  test.assertEqual(dropbox.who(), 'DropBox JS Library ! What else ?', "DropBox Library is aware of itself");
+	test.assertEqual(dropbox.who(), 'DropBox JS Library ! What else ?', "DropBox Library is aware of itself");
+};
+
+/*
+exports.testGetToken = function(test) {
+	dropbox.token('a.thieriot@gmail.com', '79a5sr13dy', function(response) {
+		test.assertEqual(response.status, '200');
+		console.log(response.json.token + "-" + response.json.secret);	
+		test.done();
+	});		
+	test.waitUntilDone(100000);
+};
+*/
+
+exports.testGetAccountInfo = function(test) {
+	dropbox.accountInfo(clientData, function(response) {
+		test.assertEqual(response.status, '200');
+		console.log(response.text);
+		console.log(response.json.display_name);	
+		test.done();
+	});		
+	test.waitUntilDone(100000);
 };
